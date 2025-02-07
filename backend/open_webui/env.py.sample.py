8eadd26a08148b80785916f7ba18397ee414cb51
kenixxie <66763093+kenixxie@users.noreply.github.com>
2025-02-07 12:47:00 +0800

    20250207
    修改了图标及端口

--- backend/open_webui/env.py.sample.py
+++ backend/open_webui/env.py.sample.py
@@ -106,9 +106,9 @@ for source in log_sources:
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "产互大模型")
-if WEBUI_NAME != "产互大模型":
-    WEBUI_NAME += " (产互大模型)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
+if WEBUI_NAME != "Open WebUI":
+    WEBUI_NAME += " (Open WebUI)"
 
 WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
 
